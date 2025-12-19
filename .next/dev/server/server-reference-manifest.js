b624cf1ae633cc9c21bf3e0d35b28975df03ca02
Aditya Raj <adityaraj0421@Adityas-MacBook-Air.local>
2025-12-20 02:37:28 +0530

    chore: Apply .gitignore rules, untrack node_modules and .next

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dxaaC/AqO9LqYUeQhsC+mySBzdLOoeqiAlUbLhm37ek=\"\n}"
--- .next/dev/static/chunks/src_app_layout_ef03be9a.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__954c703b._.css",
-  "static/chunks/_a30d8478._.js"
-],
-    source: "dynamic"
-});
